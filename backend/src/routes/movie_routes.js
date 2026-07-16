689fb91700bf2ddc0c0bbb28a8215bdacb519d53
Peducator <phamsonhoangphuc0404@gmail.com>
2026-07-16 21:53:10 +0700

    chore: config movie_routes

--- backend/src/routes/movie_routes.js
+++ backend/src/routes/movie_routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const { getAllMovies, getMovieById, createMovie, updateMovie, deleteMovie } = require("../controllers/movie_controllers");
-const { accessToken, adminMiddleware} = require("../middlewares/auth_middlewares");
+const { verifyToken, adminMiddleware} = require("../middlewares/auth_middlewares");
 
 router.get("/movies", getAllMovies);
 router.get("/movies/:id", getMovieById);
-router.post("/movies", accessToken, adminMiddleware, createMovie);
-router.put("/movies/:id", accessToken, adminMiddleware, updateMovie);
-router.delete("/movies/:id", accessToken, adminMiddleware, deleteMovie);
+router.post("/movies", verifyToken, adminMiddleware, createMovie);
+router.put("/movies/:id", verifyToken, adminMiddleware, updateMovie);
+router.delete("/movies/:id", verifyToken, adminMiddleware, deleteMovie);
 
 module.exports = router;
