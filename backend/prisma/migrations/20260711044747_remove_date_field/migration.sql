865d15102520c1fcdd80f54f4e4effd357c93519
Peducator <phamsonhoangphuc0404@gmail.com>
2026-07-18 14:28:08 +0700

    chore: backend/.gitignore

---
+++ backend/prisma/migrations/20260711044747_remove_date_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `date` on the `showtimes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `showtimes` DROP COLUMN `date`;
---
+++ backend/prisma/migrations/20260713020953_add_cooldown_until/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `cooldown_until` DATETIME(3) NULL;
